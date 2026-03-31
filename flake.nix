bc43c36811d8c36903debd00550bf2171db6c78d
Nikolos <banfen321@gmail.com>
2026-03-31 16:46:47 +0200

    fix: add root flake.nix for nix profile install

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "OmniNix - AI-Powered Nix Dev Environment Generator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "omnix";
+          version = "0.1.0";
+          src = ./.;
+          
+          # Use vendor directory to avoid needing to calculate vendorHash
+          vendorHash = null;
+
+          meta = with pkgs.lib; {
+            description = "Zero-config AI generator for Nix flake environments";
+            homepage = "https://github.com/banfen321/OmniNix";
+            license = licenses.mit;
+            mainProgram = "omnix";
+          };
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go gopls ];
+        };
+      }
+    );
+}
