098da24e65340f8814cf3387a044d640dea13c33
Nikolos <banfen321@gmail.com>
2026-03-31 16:49:55 +0200

    fix(flake): rename output binary from OmniNix to omnix

--- flake.nix
+++ flake.nix
@@ -20,6 +20,10 @@
           # Use vendor directory to avoid needing to calculate vendorHash
           vendorHash = null;
 
+          postInstall = ''
+            mv $out/bin/OmniNix $out/bin/omnix
+          '';
+
           meta = with pkgs.lib; {
             description = "Zero-config AI generator for Nix flake environments";
             homepage = "https://github.com/banfen321/OmniNix";
